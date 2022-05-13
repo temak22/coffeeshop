92d09653c6f9360b64e586379b9b8de7583b8098
temak22 <kireev.a.o@edu.mirea.ru>
2022-05-13 23:21:00 +0300

    fix commit

--- src/main/java/ru/mirea/coffeeshop/controllers/HomeController.java
+++ src/main/java/ru/mirea/coffeeshop/controllers/HomeController.java
@@ -1,24 +1,14 @@
 package ru.mirea.coffeeshop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.mirea.coffeeshop.entities.Item;
-import ru.mirea.coffeeshop.entities.User;
 import ru.mirea.coffeeshop.repositories.ItemRepository;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import ru.mirea.coffeeshop.entities.Item;
-import ru.mirea.coffeeshop.repositories.ItemRepository;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
